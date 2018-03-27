594696b7f863c37d41558e3da80471c65c216acc
Stanislav Zholobov <lesstergy@gmail.com>
2018-03-27 17:33:32 +0300

    Add base scripts, make base scene

---
+++ Assets/Scripts/ChessPiece.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChessPiece : MonoBehaviour {
+
+    public enum Type {
+        King, //Король
+        Queen, //Ферзь
+        Rook, //Ладья
+        Knight, //Конь
+        Bishop, //Слон
+        Pawn //Пешка
+    }
+
+    [SerializeField]
+    private Type _type;
+    public Type type { get { return _type; } }
+    
+    void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+}
